024ad510f9a15aff6e9cce5b9a25d12357a00c19
Sidakveer <sidakveersingh8@gmail.com>
2022-10-03 17:46:46 -0400

    create a view and set url path for testing

--- base/views.py
+++ base/views.py
@@ -1,5 +1,7 @@
+from typing import List
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic.list import ListView
+from .models import Task
 
-def taskList(request):
-    return HttpResponse("Todo List")
+class TaskList(ListView):
+    model = Task
