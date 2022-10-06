0ec75908024dcb176a90b1a46f08ad9a21be782f
Sidakveer <sidakveersingh8@gmail.com>
2022-10-05 20:08:00 -0400

    made it work 'TaskCreate view'

--- base/views.py
+++ base/views.py
@@ -2,6 +2,8 @@ from typing import List
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 from .models import Task
 
 class TaskList(ListView):
@@ -12,3 +14,10 @@ class TaskList(ListView):
 class TaskDetail(DetailView):
     model = Task
     context_object_name = "task"
+
+
+class TaskCreate(CreateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("tasks")
+
