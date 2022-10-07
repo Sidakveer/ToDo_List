69598f920f366500bb0d50e840280a3d307ee332
Sidakveer <sidakveersingh8@gmail.com>
2022-10-07 18:28:36 -0400

    adding login mixin to restrict users from viewing other users data

--- base/views.py
+++ base/views.py
@@ -7,6 +7,8 @@ from django.urls import reverse_lazy
 
 from django.contrib.auth.views import LoginView
 
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 from .models import Task
 
 
@@ -19,29 +21,34 @@ class CustomLoginView(LoginView):
         return reverse_lazy("tasks")
 
 
-class TaskList(ListView):
+class TaskList(LoginRequiredMixin, ListView):
     model = Task
     context_object_name = "tasks"
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tasks'] = context["tasks"].filter(user=self.request.user)
+        context['count'] = context["tasks"].filter(complete=False).count()
+        return context
 
-class TaskDetail(DetailView):
+class TaskDetail(LoginRequiredMixin, DetailView):
     model = Task
     context_object_name = "task"
 
 
-class TaskCreate(CreateView):
+class TaskCreate(LoginRequiredMixin, CreateView):
     model = Task
     fields = "__all__"
     success_url = reverse_lazy("tasks")
 
 
-class TaskUpdate(UpdateView):
+class TaskUpdate(LoginRequiredMixin, UpdateView):
     model = Task
     fields = "__all__"
     success_url = reverse_lazy("tasks")
 
 
-class TaskDelete(DeleteView):
+class TaskDelete(LoginRequiredMixin, DeleteView):
     model = Task
     context_object_name = "task"
     success_url = reverse_lazy("tasks")
